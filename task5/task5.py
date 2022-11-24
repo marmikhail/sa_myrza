c19a71cadaca77bc29f28dc4e9b0c4fd4485f964
Mikhail Myrzakhanov <murmisha@gmail.com>
2022-11-24 10:13:39 +0300

    new changes

--- task5/task5.py
+++ task5/task5.py
@@ -2,18 +2,15 @@ import numpy as np
 import json
 
 
-def parse_str(s_data: str):
-    levels = json.loads(s_data)
-    args = sorted(
-        np.hstack(levels),
-        key=lambda e: int(e)
-    )
-    return levels, args
+def get_dif(matrix: np.matrix):
+    diff = np.array(np.where(matrix == 0)).T
+    upper_diff = np.array([d for d in diff if d[0] < d[1]]) + 1
+    return upper_diff
 
 
-def construct_matrix(levels: list, args: list[str]):
+def constr_mtx(levels: list, args: list[str]):
     size = len(args)
-    matrix = np.zeros((size, size))
+    mtx = np.zeros((size, size))
     passed = []
 
     for level in levels:
@@ -26,65 +23,55 @@ def construct_matrix(levels: list, args: list[str]):
             for elem in level:
                 arg = int(elem) - 1
                 for idx in left:
-                    matrix[arg, int(idx) - 1] = 1
+                    mtx[arg, int(idx) - 1] = 1
                 passed.append(int(elem) - 1)
         else:
             elem = level
             arg = int(elem) - 1
             for idx in left:
-                matrix[arg, int(idx) - 1] = 1
+                mtx[arg, int(idx) - 1] = 1
             passed.append(int(elem) - 1)
 
-    return np.matrix(matrix)
+    return np.matrix(mtx)
 
 
-def merge_matrices(matrix_1: np.matrix, matrix_2: np.matrix):
+
+def str_prs(s_data: str):
+    levels = json.loads(s_data)
+    args = sorted(
+        np.hstack(levels),
+        key=lambda e: int(e)
+    )
+    return levels, args
+
+
+def dumps_result(array: np.array):
+    return json.dumps([[str(elem) for elem in pair.tolist()] for pair in array])
+
+def merge_mtx(matrix_1: np.matrix, matrix_2: np.matrix):
     return np.multiply(matrix_1, matrix_2) + np.multiply(matrix_1.T, matrix_2.T)
 
 
-def get_differences(matrix: np.matrix):
-    diff = np.array(np.where(matrix == 0)).T
-    upper_diff = np.array([d for d in diff if d[0] < d[1]]) + 1
-    return upper_diff
-
-
-def dumps_answer(array: np.array):
-    return json.dumps([[str(elem) for elem in pair.tolist()] for pair in array])
-
-
 def task(string_1: str, string_2: str):
-    levels_1, args_1 = parse_str(string_1)
-    levels_2, args_2 = parse_str(string_2)
+    lvls_1, arguments_1 = str_prs(string_1)
+    lvls_2, arguments_2 = str_prs(string_2)
 
-    matrix_a = construct_matrix(levels_1, args_1)
-    matrix_b = construct_matrix(levels_2, args_2)
+    matrix_1 = constr_mtx(lvls_1, arguments_1)
+    matrix_2 = constr_mtx(lvls_2, arguments_2)
 
-    merged_matrix = merge_matrices(matrix_A, matrix_B)
+    merged_matrix = merge_mtx(matrix_1, matrix_2)
 
-    differences = get_differences(merged_matrix)
+    dif = get_dif(merged_matrix)
 
-    answer = dumps_answer(differences)
+    result = dumps_result(dif)
 
-    return answer
+    return result
 
 
 if __name__ == "__main__":
-    range_A = '["1", ["2","3"],"4", ["5", "6", "7"], "8", "9", "10"]'
-    # range_B = '[["1","2"], ["3","4","5"], "6", "7", "9", ["8","10"]]'
-    range_B = '[["3","4"], ["1","2","5"], "7", "6", "9", ["8","10"]]'
-    # range_B = '["10","9","8","7","6","5","4","3","2","1"]'
-    # target_difference = '[["8","9"]]'
+    range_1 = '["1", ["2","3"],"4", ["5", "6", "7"], "8", "9", "10"]'
+    range_2 = '[["3","4"], ["1","2","5"], "7", "6", "9", ["8","10"]]'
 
-    levels_A, args_A = parse_str(range_A)
-    levels_B, args_B = parse_str(range_B)
+    res = task(range_1, range_2)
 
-    matrix_A = construct_matrix(levels_A, args_A)
-    matrix_B = construct_matrix(levels_B, args_B)
-
-    merged_matrix = merge_matrices(matrix_A, matrix_B)
-
-    differences = get_differences(merged_matrix)
-
-    answer = dumps_answer(differences)
-
-    print(answer)
+    print(res)
